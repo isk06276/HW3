9d4f500a5863424afb42938423efde8a9ce1bf84
Yohan-ParkGist <johnyein@gm.gist.ac.kr>
2021-06-02 21:15:10 +0900

    exit

--- syscall.c
+++ syscall.c
@@ -13,8 +13,30 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
   printf ("system call!\n");
-  thread_exit ();
+	switch (*(uint32_t *)(f->exp)){
+		case SYS_HALT:
+			break;
+		case SYS_EXIT:
+			exit(*(uint32_t)(f->esp+20));
+			break;
+
+
+
+
+
+	}
+
+}
+void halt(void){
+	power_off();
+}
+
+void exit(int status){
+	struct thread *t= thread_current();
+	t->exit_status = status;
+	print("%s: exit(%d)\n", t->name, status);
+	thread_exit();
 }
